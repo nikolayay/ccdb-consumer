305c3e2c3b4c3cf64c48b20bf0eb80e9941fd333
nikolayay <ec2-user@ip-172-31-87-216.ec2.internal>
2020-12-02 07:30:34 +0000

    hehe

--- ccdb.py
+++ ccdb.py
@@ -1,12 +1,18 @@
 import faust
 import numpy as np
+import time
 
+""" 
+TODO change names to not be explicit
+TODO scale up boxes to run pytorch + GPUs yeehaw
+     run two workers
+TODO autoscaling group for worker nodes
+TODO figure out deployment
+"""
 
 app = faust.App(
-    'goodbye-world',
+    'cum-world',
     broker='kafka://172.31.76.215:9092',
-    topic_partitions=8,
-
 )
 
 class Frame(faust.Record, serializer='json'):
@@ -14,22 +20,32 @@ class Frame(faust.Record, serializer='json'):
     frame: np.ndarray
 
 
-print(Frame(index=0, frame=np.ones(1)).dumps(serializer='pickle'))
-
-images_topic = app.topic('slags-2', value_type=Frame)
+images_topic = app.topic('slags-3', value_type=Frame)
 
 
-@app.agent(images_topic)
+# @app.agent(agent_frame_sink)
+# async def sink_it(stream):
+#     async for frames in stream.take(50, within=10):
+#         o_frames = sorted(frames, key = lambda i: i.index)
+#         print([frame.index for frame in o_frames])
+
+
+@app.agent(images_topic, concurrency=3)
 async def images(stream):
-    async for frames in stream.take(30, within=5):
+    async for frame in stream:
+
+        #todo stream processing here
+        print(frame.index)
         #print(frames)
-        o_frames = sorted(frames, key = lambda i: i.index)
-        print([frame.index for frame in o_frames])
+        # o_frames = sorted(frames, key = lambda i: i.index)
+        # print([frame.index for frame in o_frames])
         # ix = frame['index']
         # kek = np.asarray(frame['frame'])
         # print(frame.index)
         # print(np.asarray(frame.frame).shape)
         #frame = np.frombuffer(frame, np.uint8)
+
+        #todo write to s3/publish to s3 url that we show in flask 
 
 
 
